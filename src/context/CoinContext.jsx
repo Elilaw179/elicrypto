75938851adfcc26d9fd3c04baaf7ae47346e5d15
Elilaw179 <sundayelisha179@gmail.com>
2025-04-05 18:46:44 +0100

    first commit

---
+++ src/context/CoinContext.jsx
@@ -0,0 +1,42 @@
+import { createContext, useEffect, useState } from "react";
+export const CoinContext = createContext();
+
+const CoinContextprovider = (props) => {
+  const [allCoin, setAllCoin] = useState([]);
+  const [currency, setCurrency] = useState({
+    name: "usd",
+    symbol: "$",
+  });
+
+  const fetAllCoin = async () => {
+    const options = {
+      method: "GET",
+      headers: {
+        accept: "application/json",
+        "x-cg-demo-api-key": "CG-B4EYhgqGQfHNpeDtZ9pLMx2C",
+      },
+    };
+
+    fetch(
+      `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currency.name}`,
+      options
+    )
+      .then((res) => res.json())
+      .then((res) => setAllCoin(res))
+      .catch((err) => console.error(err));
+  };
+  useEffect(() => {
+    fetAllCoin();
+  }, [currency]);
+  const contextValue = {
+    allCoin,
+    currency,
+    setCurrency,
+  };
+  return (
+    <CoinContext.Provider value={contextValue}>
+      {props.children}
+    </CoinContext.Provider>
+  );
+};
+export default CoinContextprovider;
